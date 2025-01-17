a38d539545db4e5de1a9454e62d85b74999a1811
Sarthak Sarkar <22SSarkar@heckgrammar.co.uk>
2025-01-17 16:50:47 +0000

    homework

--- HowToUseGitHubClassroom/Program.cs
+++ HowToUseGitHubClassroom/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HowToUseGitHubClassroom
+﻿using System.Net.Security;
+
+namespace HowToUseGitHubClassroom
 {
     internal class Program
     {
@@ -21,6 +23,11 @@
             // Output to the console the variable
             // Output to the console Hello World
             // Test your program and add the evidence to this repository
+
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Hello World");
         }
     }
 }
